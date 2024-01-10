1406234f26ae97f2855f9f251f79a5c84ac9ae42
Nehash <nehash.j.m027@gmail.com>
2024-01-10 14:34:13 +0530

    Day4 Assignment

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4EmployeeJenkinsAssignment
+{
+    public class Product
+    {
+        public string PName { get; set; }
+        public double PPrice { get; set; }
+        public string PBrand { get; set; }
+        public DateTime ManufacturingDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day4EmployeeJenkinsAssignment;
+
+List<Product> products = new List<Product>()
+{
+    new Product(){PName="Bat",PPrice=20000,PBrand="MRF",ManufacturingDate=new DateTime(day:10,month:10,year:2022),ExpiryDate=new DateTime(day:10,month:10,year:2022)},
+    new Product(){PName="Ball",PPrice=25000,PBrand="MRF",ManufacturingDate=new DateTime(day:10,month:10,year:2022),ExpiryDate=new DateTime(day:10,month:10,year:2022)},
+    new Product(){PName="Stump",PPrice=28000,PBrand="MRF",ManufacturingDate=new DateTime(day:10,month:10,year:2022),ExpiryDate=new DateTime(day:10,month:10,year:2022)},
+    new Product(){PName="Glows",PPrice=29000,PBrand="MRF",ManufacturingDate=new DateTime(day:10,month:10,year:2022),ExpiryDate=new DateTime(day:10,month:10,year:2022)},
+    new Product(){PName="Halmet",PPrice=23000,PBrand="MRF",ManufacturingDate=new DateTime(day:10,month:10,year:2022),ExpiryDate=new DateTime(day:10,month:10,year:2022)},
+
+};
+Console.WriteLine("Name \t Price \t Brand \t ManuFacturingDate \t ExpiryDate ");
+foreach (Product product in products)
+{
+    Console.Write(product.PName+"\t");
+    Console.Write(product.PPrice + "\t");
+    Console.Write(product.PBrand+ "\t");
+    Console.Write(product.ManufacturingDate + "\t");
+    Console.Write(product.ExpiryDate + "\t");
+    Console.Write("\n");
+
+}
